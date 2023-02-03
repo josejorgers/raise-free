2375cf34616b82d829c0244c905542238ff222bd
josejorgers <josejorgexl@gmail.com>
2023-02-03 17:47:55 +0100

    fix: remove unnecessary networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,14 +12,6 @@ module.exports = {
         blockNumber: 15760547,
       },
       gasPrice: 514564026652,
-    },
-    goerli: {
-      url: process.env.ALCHEMY_GOERLI_URL,
-      accounts: [process.env.GOERLI_PRIVATE_KEY],
-    },
-    local: {
-      url: "HTTP://127.0.0.1:7545",
-      accounts: [process.env.LOCAL_PRIVATE_KEY],
     }
   },
   etherscan: {
