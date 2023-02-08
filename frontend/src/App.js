456db27d139cac75980e34e77ad4f3f662f0b78f
josejorgers <josejorgexl@gmail.com>
2023-02-08 17:45:48 +0100

    ref: remove unnecessary imports and definitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,13 @@ import Listing from './components/listing/listing';
 import { Route, Routes } from 'react-router-dom';
 import CreateFundraisingForm from './components/createForm/createForm';
 import FundraisingDetail from './components/details/details';
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers'
+import { useState } from 'react';
 import { WagmiConfig } from 'wagmi';
-import { CONTRACT_ADDRESS } from './constants';
-import abi from './contracts/Fundraiser.json';
 import { client } from './wagmiConfig';
 
 
 function App() {
   const [ address, setAddress ] = useState(null);
-  const [ contract, setContract ] = useState(null);
-  const [ provider, setProvider] = useState(null);
-  
-  
-  useEffect(() => {
-    
-    
-  }, [])
-
 
   return (
     <WagmiConfig client={ client }>
@@ -30,7 +18,7 @@ function App() {
       <TopNavbar address={address} setAddress={setAddress} />
         <main className='flex flex-col items-center justify-center w-full mt-20'>
           <Routes>
-            <Route path="/create" element={<CreateFundraisingForm contract={contract} provider={provider} />} />
+            <Route path="/create" element={<CreateFundraisingForm />} />
             <Route path="/details/:id" element={<FundraisingDetail address={address} />} />
             <Route path="/" element={ <Listing  /> } />
           </Routes>
